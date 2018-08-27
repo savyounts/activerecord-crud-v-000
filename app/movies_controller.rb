42f36dbed324dd1cd0fdb8b66c501e28731b7acd
Sav Younts <savyounts@gmail.com>
2018-08-27 18:13:33 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -37,7 +37,7 @@ release_date: 1990)
 end
 
 def can_get_the_first_item_in_the_database
-  Movie.first 
+  Movie.first
 end
 
 def can_get_the_last_item_in_the_database
@@ -45,7 +45,7 @@ def can_get_the_last_item_in_the_database
 end
 
 def can_get_size_of_the_database
-  __
+  Movie.all.size
 end
 
 def can_find_the_first_item_from_the_database_using_id
