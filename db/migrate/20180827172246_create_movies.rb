f7afdc3435b1c60f797012bdd72318b96af8e9e6
Sav Younts <savyounts@gmail.com>
2018-08-27 17:31:46 +0000

    automatically backed up by learn

--- db/migrate/20180827172246_create_movies.rb
+++ db/migrate/20180827172246_create_movies.rb
@@ -4,6 +4,6 @@ class CreateMovies < ActiveRecord::Migration[5.2]
     add_column :movies, :release_date, :integer,
     add_column :movies, :director, :string,
     add_column :movies, :lead, :string,
-    add_column :movies, :in_theaters, :boolean 
+    add_column :movies, :in_theaters, :boolean
   end
 end
